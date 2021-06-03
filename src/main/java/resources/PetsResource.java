0d997839edbfbec37631df170e7778b3e70799a3
Esteban <lecardenasc@unbosque.edu.co>
2021-06-02 21:14:09 -0500

    punto 10 arreglado

--- src/main/java/resources/PetsResource.java
+++ src/main/java/resources/PetsResource.java
@@ -3,6 +3,7 @@ package resources;
 import resources.pojos.Owner;
 import resources.pojos.Pet;
 import resources.pojos.Vet;
+import resources.pojos.Visit;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
@@ -14,6 +15,7 @@ import java.util.List;
 
 public class PetsResource {
     List<Pet> petList = new ArrayList<Pet>();
+    List<Visit> visitsList = new ArrayList<Visit>();
     public void data(){
         petList.add(new Pet(1, "1526HA" , "Felix", "Gato", "angola", "PEQUEÑO", "MACHO", "" , 1));
         petList.add(new Pet(2, null , "Bellota", "Perro", "Criollo", "GRANDE", "HEMBRA", "" , 1));
@@ -27,6 +29,20 @@ public class PetsResource {
         petList.add(new Pet(10, "876GAN" , "Lupe", "Perro", "Criollo", "GRANDE", "HEMBRA", "" ,5));
         petList.add(new Pet(11, "098GJA" , "Tony", "Loro", "tricahues", "PEQUEÑO", "MACHO", "" ,6));
         petList.add(new Pet(12, null , "Lola", "Tortuga", "Mediterránea", "GRANDE", "HEMBRA", "" ,6));
+    }
+    public void dataVisit(){
+        visitsList.add(new Visit(1, "2001-02-25", "vacunacion", "la primera vacuna del gato", 1, 2));
+        visitsList.add(new Visit(2, "2020-06-10", "microchip", "le implementaran el microchip al animal", 3, 1));
+        visitsList.add(new Visit(3, "2021-05-31", "microchip", "le implementaran el microchip al animal", 2, 3));
+        visitsList.add(new Visit(4, "2021-08-15", "microchip", "le implementaran el microchip al animal", 2, 4));
+        visitsList.add(new Visit(5, "2021-09-01", "esterilización", "el animal sufrio se esterilizo", 2, 5));
+        visitsList.add(new Visit(6, "2021-03-28", "esterilización", "el animal sufrio se esterilizo", 2, 6));
+        visitsList.add(new Visit(7, "2021-03-28", "esterilización", "el animal sufrio se esterilizo", 2, 7));
+        visitsList.add(new Visit(8, "2021-03-28", "microchip", "le implementaran el microchip al animal", 2, 8));
+        visitsList.add(new Visit(9, "2021-03-28", "microchip", "le implementaran el microchip al animal", 2, 9));
+        visitsList.add(new Visit(10, "2021-03-28", "microchip", "le implementaran el microchip al animal", 2, 10));
+        visitsList.add(new Visit(11, "2021-03-28", "microchip", "le implementaran el microchip al animal", 2, 11));
+        visitsList.add(new Visit(12, "2021-03-28", "esterilización", "el animal sufrio se esterilizo", 2, 12));
     }
 
     @POST
@@ -135,15 +151,23 @@ public class PetsResource {
     @Produces(MediaType.APPLICATION_JSON)
     public Response listMicrochip(@PathParam("microchip") Boolean microchip ) {
         data();
+        dataVisit();
         List<Pet> petList2 = new ArrayList<Pet>();
         for (Pet pet : petList){
-            if(microchip == true){
-                if(pet.getMicroship() != null){
-                    petList2.add(pet);
+            for ( Visit visit : visitsList){
+                if(microchip == true) {
+                    if(visit.getType().equals("microchip")){
+                        if(pet.getPet_id().equals(visit.getPet_id())){
+                            petList2.add(pet);
+                        }
+                    }
                 }
-            }else{
-                if(pet.getMicroship() == null){
-                    petList2.add(pet);
+                if(microchip == false) {
+                    if(!visit.getType().equals("microchip")){
+                        if(pet.getPet_id().equals(visit.getPet_id())){
+                            petList2.add(pet);
+                        }
+                    }
                 }
             }
         }
@@ -151,5 +175,38 @@ public class PetsResource {
                 .entity(petList2)
                 .build();
     }
+
+    @GET
+    @Path("pet/esterilizacion/{esterilizacion}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response listEsterilizacion(@PathParam("esterilizacion") Boolean esterilizacion ) {
+        data();
+        dataVisit();
+        List<Pet> petList2 = new ArrayList<Pet>();
+        for (Pet pet : petList){
+            for ( Visit visit : visitsList){
+                if(esterilizacion == true) {
+                    if(visit.getType().equals("esterilización")){
+                        if(pet.getPet_id().equals(visit.getPet_id())){
+                            petList2.add(pet);
+                        }
+                    }
+                }
+                if(esterilizacion == false) {
+                    if(!visit.getType().equals("esterilización")){
+                        if(pet.getPet_id().equals(visit.getPet_id())){
+                            petList2.add(pet);
+                        }
+                    }
+                }
+
+            }
+        }
+        return Response.ok()
+                .entity(petList2)
+                .build();
+    }
+
+
 
 }
